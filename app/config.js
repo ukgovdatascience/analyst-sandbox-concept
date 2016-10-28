dac2c6410bc6d0f59ae7abb2beefb0d2aab29196
Prateek Buch <prateek.buch@digital.cabinet-office.gov.uk>
2016-10-28 15:35:13 +0100

    Change service name

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Service name goes here",
+  serviceName: "Analyst sandbox prototype",
 
   // Default port that prototype runs on
   port: '3000',
