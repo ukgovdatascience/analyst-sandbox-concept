6abeb79cdbffce64d9a8be9f1a7a1be886817e84
Prateek Buch <prateek.buch@digital.cabinet-office.gov.uk>
2016-11-07 16:10:02 +0000

    Amend advBegin branch vi

---
+++ app/routes-copy.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+  
+  res.render('index');
+
+});
+
+
+// Example routes - feel free to delete these
+
+// Passing data into a page
+
+router.get('/examples/template-data', function (req, res) {
+
+  res.render('examples/template-data', { 'name' : 'Foo' });
+
+});
+
+// Branching
+
+router.get('/adv-user', function (req, res) {
+
+  // get the answer from the query string (eg. ?over18=false)
+  var advuser = req.query.advuser;
+
+  if (advuser == "false"){
+
+    // redirect to the relevant page
+    res.redirect("/choose-tools");
+
+  } else {
+
+    // if over18 is any other value (or is missing) render the page requested
+    res.render('/beginner');
+
+  }
+
+});
+
+// add your routes here
+
+module.exports = router;
