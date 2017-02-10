46ea46b50e2e32b2787162a4b64f21b572845e0e
Prateek Buch <prateek.buch@digital.cabinet-office.gov.uk>
2017-02-10 10:58:04 +0000

    refresh prototype to reset docs routs ts

--- docs/documentation_routes.js
+++ docs/documentation_routes.js
@@ -45,16 +45,16 @@ router.get('/examples/template-data', function (req, res) {
 
 // Branching
 
-router.get('/app/views/existing-sandboxes', function (req, res) {
+router.get('/examples/over-18', function (req, res) {
   // get the answer from the query string (eg. ?over18=false)
   var over18 = req.query.over18
-console.log (over18);
+
   if (over18 === 'false') {
     // redirect to the relevant page
-    res.redirect('/app/views/adv-branch')
+    res.redirect('/docs/examples/under-18')
   } else {
     // if over18 is any other value (or is missing) render the page requested
-    res.render('/app/views/existing-sandboxes')
+    res.render('examples/over-18')
   }
 })
 
