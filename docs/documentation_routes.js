f52e744b1b496e696a4231d20900db308df3e513
Prateek Buch <prateek.buch@digital.cabinet-office.gov.uk>
2017-02-10 10:26:17 +0000

    amend documentations routes js

--- docs/documentation_routes.js
+++ docs/documentation_routes.js
@@ -45,16 +45,16 @@ router.get('/examples/template-data', function (req, res) {
 
 // Branching
 
-router.get('/examples/over-18', function (req, res) {
+router.get('/app/views/existing-sandboxes', function (req, res) {
   // get the answer from the query string (eg. ?over18=false)
   var over18 = req.query.over18
 
   if (over18 === 'false') {
     // redirect to the relevant page
-    res.redirect('/docs/examples/under-18')
+    res.redirect('/app/views/adv-branch')
   } else {
     // if over18 is any other value (or is missing) render the page requested
-    res.render('examples/over-18')
+    res.render('/app/views/existing-sandboxes')
   }
 })
 
