6987d9eb38d067153ade838adc6f1217abce36d6
Prateek Buch <prateek.buch@digital.cabinet-office.gov.uk>
2017-02-10 10:37:53 +0000

    console js

--- docs/documentation_routes.js
+++ docs/documentation_routes.js
@@ -48,7 +48,7 @@ router.get('/examples/template-data', function (req, res) {
 router.get('/app/views/existing-sandboxes', function (req, res) {
   // get the answer from the query string (eg. ?over18=false)
   var over18 = req.query.over18
-
+console.log (over18);
   if (over18 === 'false') {
     // redirect to the relevant page
     res.redirect('/app/views/adv-branch')
